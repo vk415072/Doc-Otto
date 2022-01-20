f1b9afebd5f82521c68b85afef1b52f5d4236172
Vivek Sengar <vk415072@gmail.com>
2022-01-20 18:45:07 +0100

    react app and i quill
    created react app and installed quilljs (basic ini)

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+// 2. importing Editor.js
+import Editor from "./Editor";
+
+function App() {
+  // 3. returning the Editor.js
+  return <Editor/>
+}
+
+export default App;
---
+++ client/src/Editor.js
@@ -0,0 +1,8 @@
+// 1. create a file for all code of text editor
+import React from 'react';
+
+export default function Editor() {
+  return <div>
+      This is the text editor
+  </div>;
+}
